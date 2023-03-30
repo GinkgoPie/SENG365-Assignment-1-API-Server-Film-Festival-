92c1bd7739abb5d5a098934c7eae609c866a7b0c
Jiahe <jba214@uclive.ac.nz>
2023-03-30 22:29:36 +1300

    films

--- src/app/controllers/film.server.controller.ts
+++ src/app/controllers/film.server.controller.ts
@@ -1,28 +1,18 @@
 import {Request, Response} from "express";
 import * as films from "../models/film.server.model";
 import * as schemas from '../resources/schemas.json';
-import Ajv from "ajv";
-import addFormats from "ajv-formats";
 import Logger from "../../config/logger";
+import {validate} from '../validate';
+import {ParamsDictionary} from "express-serve-static-core";
 
 
-const ajv = new Ajv({removeAdditional: 'all', strict: false});
-addFormats(ajv)
-
-const validate = async (schema: object, data: any) => {
-    try {
-        const validator = ajv.compile(schema);
-        const valid = await validator(data);
-        if(!valid)
-            return ajv.errorsText(validator.errors); return true;
-    } catch (err) {
-        return err.message;
-    }
+const isNumeric = async (value:any)=> {
+    return /^-?\d+$/.test(value);
 }
 
 const buildQuery = async (req: Request) => {
 
-    let sql = 'select f.id as filmId, title, genre_id as genreId, director_id as directorId, first_name as directorFirstName, last_name as directorLastName,release_date as releaseDate, age_rating as ageRating, rating from (film f left join (select round(avg(rating),1) as rating, user_id as reviewer_id, film_id from film_review group by film_id ) r on f.id = r.film_id) left join user d on director_id = d.id';
+    let sql = 'select f.id as filmId, title, genre_id as genreId, director_id as directorId, first_name as directorFirstName, last_name as directorLastName,release_date as releaseDate, age_rating as ageRating, (coalesce(rating, 0)) as rating from (film f left join (select round(avg(coalesce(rating, 0)),1) as rating, user_id as reviewer_id, film_id from film_review group by film_id ) r on f.id = r.film_id) left join user d on director_id = d.id';
     sql = sql + ' where '
     if (req.query.q) {
         sql = sql + '(title like "%' + req.query.q+ '%" or description like "%' + req.query.q+ '%") and ';
@@ -33,20 +23,26 @@ const buildQuery = async (req: Request) => {
     if (req.query.reviewerId) {
         sql = sql + 'reviewer_id=' + req.query.reviewerId + ' and ';
     }
-    if (req.query.genreIds.length>1) {
-        const genreIds  = req.query.genreIds as string[];
-        sql = sql + '(genre_id=' + genreIds[0] + ' or genre_id=' +genreIds[1] + ') and ';
+    if (req.query.genreIds !== undefined) {
+        if (req.query.genreIds.length>1) {
+            const genreIds  = req.query.genreIds as string[];
+            sql = sql + '(genre_id=' + genreIds[0] + ' or genre_id=' +genreIds[1] + ') and ';
+        }
     }
-    if (req.query.ageRatings.length>1) {
-        const ageRatings  = req.query.ageRatings as string[];
-        sql = sql + '(age_rating ="' + ageRatings[0] + '" or age_rating ="'+ ageRatings[1] +'")';
+
+    if (req.query.ageRatings !== undefined) {
+        if (req.query.ageRatings.length > 1) {
+            const ageRatings = req.query.ageRatings as string[];
+            sql = sql + '(age_rating ="' + ageRatings[0] + '" or age_rating ="' + ageRatings[1] + '")';
+        }
     }
 
     if (sql.endsWith(' and ')){
         sql = sql.slice(0, -5);
     }
-
-    sql = sql + ' group by film_id';
+    if (sql.endsWith('where ')){
+        sql = sql.slice(0, -6);
+    }
     if (req.query.sortBy) {
         const sort: {[key: string]: string} = {"ALPHABETICAL_ASC": "title ASC",
             "ALPHABETICAL_DESC" :"title DESC",
@@ -54,7 +50,10 @@ const buildQuery = async (req: Request) => {
             "RELEASED_DESC" : "releaseDate DESC",
             "RATING_ASC": "rating ASC",
             "RATING_DESC" : "rating DESC"};
-        sql = sql + ' order by ' + sort[req.query.sortBy as string];
+        sql = sql + ' order by ' + sort[req.query.sortBy as string] + ', f.id ASC';
+    }
+    else if (req.query.sortBy === undefined) {
+        sql = sql + ' order by ' + 'releaseDate ASC'
     }
     Logger.info(sql)
     return sql;
@@ -65,35 +64,79 @@ const buildQuery = async (req: Request) => {
 
 
 const viewAll = async (req: Request, res: Response): Promise<void> => {
-    // const validation = await validate(
-    //     schemas.film_search,
-    //     req.body);
-    // if (validation !== true) {
-    //     res.statusMessage = `Bad Request: ${validation.toString()} `;
-    //     res.status(400).send();
-    //     return;
-    // }
-    try{
-        if (!req.params){
-            const result = await films.getAllFilms();
-            for (const row of result) {
-                if (row.rating !== null) {
-                    row.rating = Number(row.rating);
+    const validation = await validate(
+        schemas.film_search,
+        req.query)
+    Logger.info(validation);
+    if (validation !== true) {
+        res.statusMessage = `Bad Request: ${validation.toString()} `;
+        res.status(400).send();
+        return;
+    } else{
+        if (req.query.startIndex !== undefined && isNaN(req.query.startIndex as any)){
+            res.status(400).send();
+            return;
+        }else if (req.query.count !== undefined && isNaN(req.query.count as any)){
+            res.status(400).send();
+            return;
+        }else if (req.query.directorId !== undefined && isNaN(req.query.directorId as any)){
+            res.status(400).send();
+            return;
+        }else if (req.query.reviewerId !== undefined && isNaN(req.query.reviewerId as any)){
+            res.status(400).send();
+            return;
+        }else if (req.query.genreIds !== undefined) {
+            if (Number(req.query.genreIds) > 12 || Number(req.query.genreIds) < 1) {
+                res.status(400).send();
+                return;}
+            for (const genreId of req.query.genreIds as string[]) {
+                if (isNaN(genreId as any)) {
+                    res.status(400).send();
+                    return;
+                } else if (Number(genreId) > 12 || Number(genreId) < 1) {
+                    res.status(400).send();
+                    return;
                 }
             }
-            res.status(200).send({"films":result, "count":result.length});
-            return;
-        } else {
+        }
+    }
+    let startIndex = 0;
+    if(req.query.startIndex){
+        startIndex = parseInt(req.query.startIndex as string, 10)
+    }
+    try{
+        if (req.query.q || req.query.directorId ||req.query.reviewerId ||req.query.startIndex ||req.query.count ||req.query.sortBy ||req.query.genreIds ||req.query.ageRatings ){
+            Logger.info("GET film with parameters")
             const query = buildQuery(req);
             const result = await films.searchWithSql(await query);
-            const start = parseInt(req.query.start as string,10)
-            const count = parseInt(req.query.count as string,10)
+            let count = result.length;
+            if(req.query.count){
+                count = parseInt(req.query.count as string,10)
+            }
             for (const row of result) {
-                if (row.rating !== null) {
+                if (row.rating !== 0.0) {
                     row.rating = Number(row.rating);
+                } else {
+                    row.rating = 0
                 }
             }
-            res.status(200).send({"films":result.slice(start,start+count), "count":result.length});
+            res.status(200).send({"films":result.slice(startIndex,startIndex+count), "count":result.length});
+            return;
+        } else {
+            Logger.info("GET all films")
+            const result = await films.getAllFilms();
+            let count = result.length;
+            if(req.query.count){
+                count = parseInt(req.query.count as string,10)
+            }
+            for (const row of result) {
+                if (row.rating !== 0.0) {
+                    row.rating = Number(row.rating);
+                } else {
+                    row.rating = 0
+                }
+            }
+            res.status(200).send({"films":result.slice(startIndex,startIndex+count), "count":result.length});
             return;
         }
     } catch (err) {
@@ -106,9 +149,13 @@ const viewAll = async (req: Request, res: Response): Promise<void> => {
 
 const getOne = async (req: Request, res: Response): Promise<void> => {
     try{
-        // Your code goes here
-        res.statusMessage = "Not Implemented Yet!";
-        res.status(501).send();
+        const result = await films.getFilmById(parseInt(req.params.id, 10));
+        if (result[0] === undefined) {
+            res.status(404).send("Film not found");
+            return;
+        }
+        result[0].rating = Number(result[0].rating);
+        res.status(200).send(result[0]);
         return;
     } catch (err) {
         Logger.error(err);
@@ -118,11 +165,30 @@ const getOne = async (req: Request, res: Response): Promise<void> => {
     }
 }
 
-const addOne = async (req: Request, res: Response): Promise<void> => {
+const addOne = async (req: Request, res: Response): Promise<void> => {        const datetime = new Date();
+    const date = ("0" + datetime.getDate()).slice(-2);
+    const month = ("0" + (datetime.getMonth() + 1)).slice(-2);
+    const year = datetime.getFullYear();
+    const hours = datetime.getHours();
+    const minutes = datetime.getMinutes();
+    const seconds = datetime.getSeconds();
+    const currentTime = year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds;
+    if (req.body.releaseDate < currentTime ){
+        res.statusMessage = `releaseDate in past `;
+        res.status(403).send();
+    }
+    const validation = await validate(
+        schemas.film_post,
+        req.body)
+    if (validation !== true) {
+        res.statusMessage = `Bad Request: ${validation.toString()} `;
+        res.status(400).send();
+        return;
+    }
     try{
         // Your code goes here
         res.statusMessage = "Not Implemented Yet!";
-        res.status(501).send();
+        res.status(200).send();
         return;
     } catch (err) {
         Logger.error(err);
@@ -162,9 +228,8 @@ const deleteOne = async (req: Request, res: Response): Promise<void> => {
 
 const getGenres = async (req: Request, res: Response): Promise<void> => {
     try{
-        // Your code goes here
-        res.statusMessage = "Not Implemented Yet!";
-        res.status(501).send();
+        const result = await films.listAllGenres();
+        res.status(200).send(result);
         return;
     } catch (err) {
         Logger.error(err);
--- src/app/models/film.server.model.ts
+++ src/app/models/film.server.model.ts
@@ -22,8 +22,9 @@ const getFilmById = async (id: number) : Promise<Film[]> => {
     Logger.info( `Getting film with id ${id} from the database` );
     const conn = await getPool().getConnection();
     conn.escape();
-    const query = 'select * from film where id = ?';
+    const query = 'select f.id as filmId, title, description, genre_id as genreId, director_id as directorId,u.first_name as directorFirstName, u.last_name as directorLastName,release_date as releaseDate, age_rating as ageRating, runtime, round(avg(coalesce(rating, 0)),1) as rating, count(r.id) as numReviews from (film f left join film_review r on f.id = r.film_id) left join user u on f.director_id = u.id where f.id = ? group by f.id'
     const [ rows ] = await conn.query(query,[id] );
+    rows.rating = Number(rows.rating)
     await conn.release();
     return rows;
 };
@@ -31,10 +32,19 @@ const getFilmById = async (id: number) : Promise<Film[]> => {
 const getAllFilms = async () : Promise<Film[]> => {
     Logger.info( `Getting all films from the database` );
     const conn = await getPool().getConnection();
-    const query = 'select f.id as filmId, title, genre_id as genreId, director_id as directorId,u.first_name as directorFirstName, u.last_name as directorLastName,release_date as releaseDate, age_rating as ageRating, round(avg(rating), 1) as rating from (film f left join film_review r on f.id = r.film_id) left join user u on f.director_id = u.id group by f.id order by releaseDate;'
+    const query = 'select f.id as filmId, title, genre_id as genreId, director_id as directorId,u.first_name as directorFirstName, u.last_name as directorLastName,release_date as releaseDate, age_rating as ageRating, round(avg(coalesce(rating, 0)),1) as rating from (film f left join film_review r on f.id = r.film_id) left join user u on f.director_id = u.id group by f.id order by releaseDate;'
     const [ rows ] = await conn.query(query);
     await conn.release();
     return rows;
 };
 
-export { getFilmById, getAllFilms, searchWithSql}
+const listAllGenres = async (): Promise<any[]>=> {
+    Logger.info( `Getting all genres from the database` );
+    const conn = await getPool().getConnection();
+    const query =  'select id as genreId, name from genre';
+    const [ rows ] = await conn.query(query);
+    await conn.release();
+    return rows;
+}
+
+export { getFilmById, getAllFilms, searchWithSql, listAllGenres}
